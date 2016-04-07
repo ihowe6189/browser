56f676db4b206224aa1a11a8dcb80d61dff15fe8
ihowe6189 <ihowe6189@stu.d214.org>
2016-04-07 10:02:38 -0500

    Day 1

--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView
+    
+    @IBOutlet weak var barView: UIView!
+    
+    required init(coder aDecocer: NSCoder) {
+        self.webView = WKWebView(frame: CGRectZero)
+        super.init(coder: aDecocer)!
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        barView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
+        
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let height = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0)
+        let width = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
+        view.addConstraints([height,width])
+        
+        //Default URL when the app starts
+        let url = NSURL(string: "https://www.google.com")
+        let request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        barView.frame = CGRect(x: 0, y: 0, width: size.width, height: 30)
     }
 
 
+    
 }
 
