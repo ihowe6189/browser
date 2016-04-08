25b7dd5087f09730debecb6bfa8e1f4428d50b0e
ihowe6189 <ihowe6189@stu.d214.org>
2016-04-08 09:15:47 -0500

    Day 2

--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     var webView: WKWebView
     
     @IBOutlet weak var barView: UIView!
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
     
     required init(coder aDecocer: NSCoder) {
         self.webView = WKWebView(frame: CGRectZero)
@@ -30,17 +34,49 @@ class ViewController: UIViewController {
         let width = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
         view.addConstraints([height,width])
         
+        webView.addObserver(self, forKeyPath: "loading", options: .New, context: nil)
+        
         //Default URL when the app starts
         let url = NSURL(string: "https://www.google.com")
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
+        
+        backButton.enabled = false
+        forwardButton.enabled = false
     }
 
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         barView.frame = CGRect(x: 0, y: 0, width: size.width, height: 30)
     }
 
-
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        urlField.resignFirstResponder()
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: urlField.text!)!))
+        return false
+    }
+    
+    @IBAction func back(sender: UIBarButtonItem) {
+        webView.goBack()
+    }
+    
+    @IBAction func forward(sender: UIBarButtonItem) {
+        webView.goForward()
+    }
+    
+    @IBAction func reload(sender: UIBarButtonItem) {
+        let request = NSURLRequest(URL: webView.URL!)
+        webView.loadRequest(request)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "loading" {
+            backButton.enabled = webView.canGoBack
+            forwardButton.enabled = webView.canGoForward
+        }
+        if keyPath == "estimated progress"{
+            //progressView.hidden = webView.estimatedProgress
+        }
+    }
     
 }
 
